6ea6fae741475297ded03341f7b4b4f0a4e93ad0
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-22 08:08:51 -0400

    add mock data and app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { useState } from 'react';
+import data from "./mock-data.json";
 
 function App() {
+  const [users, setUsers] = useState(data.slice(0, 30));
+
   return (
     <div className="App">
-      
+      {users.map((user) => {
+        return (
+          <div className="user">
+           <h3>{user.firstName}</h3>
+           <h3>{user.lastName}</h3>
+           <h3>{user.email}</h3>
+          </div>  
+        );
+      })}
     </div>
   );
 }
