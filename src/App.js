402130b90a81df396c3042fccb5ceb24c4864158
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-22 08:50:17 -0400

    add css and make pagination to work

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import data from "./mock-data.json";
 import ReactPaginate from 'react-paginate';
 
 function App() {
-  const [users, setUsers] = useState(data.slice(0, 30));
+  const [users, setUsers] = useState(data.slice(0, 50));
   const [pageNumber, setPageNumber] = useState(0)
 
-  const usersPerPage = 10
+  const usersPerPage = 5
   const pagesVisited = pageNumber * usersPerPage 
   
   const displayUsers = users
@@ -22,9 +22,26 @@ function App() {
     );
   });
 
+  const pageCount = Math.ceil(users.length / usersPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected)
+  }
+
   return (
     <div className="App">
-      {displayUsers}
+      {displayUsers} 
+      <ReactPaginate 
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={"paginationButtons"}
+        previousLinkClassName={"previousButton"}
+        nextLinkClassName={"nextButton"}
+        disabledClassName={"paginationDiabled"}
+        activeClassName={"paginationActive"}
+      />
     </div>
   );
 }
